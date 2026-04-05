9b462a246be53fc8d556d3d96041be4cf9ae3381
Heilotia <47873942+HeilotiaStudio@users.noreply.github.com>
2026-04-06 00:56:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener("push", event => {
   const data = event.data ? event.data.json() : {};
   const title = data.title || "Notification";
   const options = {
-    body: data.body || "Someone registered!",
+    body: data.body || "Service worker registered!",
     icon: data.icon || "/TOHAcademy/img/favicon.png"
   };
 
